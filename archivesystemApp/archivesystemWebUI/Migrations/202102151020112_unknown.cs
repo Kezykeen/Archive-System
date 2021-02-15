c291bf951b14b2d382ad45d8f9031edfdfb19d1a
munafs2301 <marvelousfrank5@gmail.com>
2021-02-15 11:41:36 +0100

    Fixes on AccessLevelController

---
+++ archivesystemApp/archivesystemWebUI/Migrations/202102151020112_unknown.cs
@@ -0,0 +1,21 @@
+namespace archivesystemWebUI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class unknown : DbMigration
+    {
+        public override void Up()
+        {
+           
+            
+           
+            
+        }
+        
+        public override void Down()
+        {
+          
+        }
+    }
+}
