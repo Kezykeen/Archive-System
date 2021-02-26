6092cc11b7a3bea5268698911981d9dea79cb26a
chidiebube <onahchidiebube@gmail.com>
2021-02-26 08:14:02 +0100

    sidebar adjustment

---
+++ archivesystemApp/archivesystemWebUI/Migrations/202102260233439_newModelsFromTeam.cs
@@ -0,0 +1,18 @@
+namespace archivesystemWebUI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newModelsFromTeam : DbMigration
+    {
+        public override void Up()
+        {
+           
+        }
+        
+        public override void Down()
+        {
+          
+        }
+    }
+}
