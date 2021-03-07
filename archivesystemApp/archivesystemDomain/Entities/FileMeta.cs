ae4a705ef64bc4f66d06ae673f58d84aa79ce8dc
chidiebube <onahchidiebube@gmail.com>
2021-03-07 16:08:00 +0100

    Implementing File Upload

---
+++ archivesystemApp/archivesystemDomain/Entities/FileMeta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace archivesystemDomain.Entities
+{
+    public class FileMeta
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public File File { get; set; }
+        public int FolderId { get; set; }
+        public Folder Folder { get; set; }
+        public int UploadedById { get; set; }
+        public Employee UploadedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
