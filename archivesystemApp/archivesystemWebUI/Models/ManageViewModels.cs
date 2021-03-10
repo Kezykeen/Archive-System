d673899df2e7362785750e8ffaaeb90f4d2d215c
kezyApps <k.okoro@genesystechhub.com>
2021-03-10 13:26:46 +0100

    Fixed bug in change password functionality

--- archivesystemApp/archivesystemWebUI/Models/ManageViewModels.cs
+++ archivesystemApp/archivesystemWebUI/Models/ManageViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
@@ -35,18 +36,19 @@ namespace archivesystemWebUI.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 
     public class ChangePasswordViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Enter your old password")]
         [DataType(DataType.Password)]
+        [Remote("ValidateOldPassword", "Manage", HttpMethod = "POST", ErrorMessage = "Incorrect password")]
         [Display(Name = "Old password")]
         public string OldPassword { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Enter your new password")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
@@ -54,7 +56,7 @@ namespace archivesystemWebUI.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 
