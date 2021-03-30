1ba181108e68af45236084f3abe4455db1adb14e
munafs2301 <marvelousfrank5@gmail.com>
2021-03-30 12:53:19 +0100

    Created features for the Dashboard

---
+++ archivesystemApp/archivesystemWebUI/Models/CardsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using archivesystemDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace archivesystemWebUI.Models
+{
+    public class CardsViewModel
+    {
+        public IEnumerable<AppUser> Appusers { get; set; }
+        public IEnumerable<Department> Departments { get; set; }
+        public IEnumerable<AccessDetail> AccessDetails { get; set; }
+        public IEnumerable<Faculty> Faculties { get; set; }
+        public IEnumerable<ApplicationRole> Roles { get; set; }
+    }
+}
